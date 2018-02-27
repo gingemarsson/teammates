ce95c89c5d45d7b4e7beb96beb6bc921a9539cec
Filipjansson95 <filipjansson0@gmail.com>
2018-02-27 14:38:08 +0100

    Test: Added AdminSessionsPageActionTest:: checkAllParametersTestNotNull (Resolves #45) (#75)
    Tests the behavior of AdminSessionsPage::checkAllParameters with "notNull" as condition.
    Resolves #45

--- src/test/java/teammates/test/cases/action/AdminSessionsPageActionTest.java
+++ src/test/java/teammates/test/cases/action/AdminSessionsPageActionTest.java
@@ -3,7 +3,7 @@ package teammates.test.cases.action;
 import org.testng.annotations.Test;
 import teammates.common.util.Const;
 import teammates.ui.controller.AdminSessionsPageAction;
-/*TestClass for AdminSessionsPageAction */
+
 public class AdminSessionsPageActionTest extends BaseActionTest{
     @Override
     protected String getActionUri() {
@@ -37,5 +37,26 @@ public class AdminSessionsPageActionTest extends BaseActionTest{
 
        boolean b = a.checkAllParameters("null");
        assertTrue(b);
+    }
+
+    @Test
+    public void checkAllParametersTestNotNull(){
+        //checkAllParmas("notNull") should return true when ALL values of
+        // startDate,endDate,startHour,endHour,startMin,endMin and timeZone
+        // are separated from null and the whitespace character("")
+
+        //All values are separated from null and whitespace, should be true.
+        AdminSessionsPageAction a = getAction("startdate", "notNull", "enddate", "notNull", "starthour", "notNull",
+                "endhour", "notNull", "startminute", "notNull", "endminute", "notNull",
+                "timezone", "notNull");
+
+        //timezone == "" should be false
+        AdminSessionsPageAction b = getAction("startdate", "notNull", "enddate", "notNull", "starthour", "notNull",
+                "endhour", "notNull", "startminute", "notNull", "endminute", "notNull",
+                "timezone", "");
+        boolean c = a.checkAllParameters("notNull");
+        boolean d = b.checkAllParameters("notNull");
+        assertTrue(c);
+        assertFalse(d);
     }
 }
